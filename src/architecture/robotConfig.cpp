e8db614f1e36f83271f39b32d1d3017a33f70e6a
Jackson Scott <bubbarooski08@gmail.com>
2025-09-24 19:47:39 -0500

    Update robotconfig

--- src/architecture/robotConfig.cpp
+++ src/architecture/robotConfig.cpp
@@ -4,5 +4,21 @@ brain Brain;
 
 controller Controller = controller(controllerType::primary);
 
+/*
+ TO-DO:
+   o- Initialize all vex devices here (motors/sensors/pneumatics/etc)
+   o- Initialize constants
+*/
+
+//-------
+
+// Constants: PLACE HERE [regularly]
+
+// Devices: PLACE HERE [regularly]
+
+//-------
+
+void vexcodeInit() {
+
 void vexcodeInit() {
 };
